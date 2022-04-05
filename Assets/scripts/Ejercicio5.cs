578762e2f2907b79e9584f4b2acff056f2a97647
SantinoPaolini <101577619+SantinoPaolini@users.noreply.github.com>
2022-04-05 10:26:56 -0300

    ejercicio 5

---
+++ Assets/scripts/Ejercicio5.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int num1;
+    public int num2;
+
+    void Start()
+    {
+
+        int res;
+        res = num1 / num2;
+        if (num2 == 0)
+        {
+
+            Debug.Log("No se puede dividir por cero");
+
+        }
+        else
+        {
+            Debug.Log("El resultado de la division entre " + num1 + "y " + num2 + "Es igual a " + res);
+        }
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
